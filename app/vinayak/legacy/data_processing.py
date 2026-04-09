78fa90f4e2dc9f3595d485d6f1627b2cc41cbe27
chandudevop86 <chandudevop@gmail.com>
2026-04-09 13:21:47 +0530

    modifyed with ltp verson2
    # Conflicts:
    #	data/crisis_risk_summary.json

--- app/vinayak/legacy/data_processing.py
+++ app/vinayak/legacy/data_processing.py
@@ -2,5 +2,15 @@
 
 from __future__ import annotations
 
-from src.data_processing import REQUIRED_OHLCV_COLUMNS, load_and_process_ohlcv as load_legacy_ohlcv_processing
+try:
+    from vinayak.data.data_processing import (
+        REQUIRED_OHLCV_COLUMNS,
+        load_and_process_ohlcv as load_legacy_ohlcv_processing,
+    )
+except ModuleNotFoundError:
+    from src.data_processing import (
+        REQUIRED_OHLCV_COLUMNS,
+        load_and_process_ohlcv as load_legacy_ohlcv_processing,
+    )
+
 __all__ = ["REQUIRED_OHLCV_COLUMNS", "load_legacy_ohlcv_processing"]
